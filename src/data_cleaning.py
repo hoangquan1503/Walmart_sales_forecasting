465d367b4711df43332b2c9ea5b69650ef4ceb31
hoangquan1503 <hoangquan.20060315.808@gmail.com>
2026-07-24 23:20:00 +0700

    Initial commit

---
+++ src/data_cleaning.py
@@ -0,0 +1,10 @@
+def check_missing_value(df):
+    df_na = pd.DataFrame({
+        "counts": df.isna().sum(),
+        "ratio": df.isna().sum() / df.shape[0]
+    })
+    return df_na
+def fill_missing_promote(cols):
+    df_fill = df.copy()
+    df_fill = df_fill.fillna(0)
+    return df_fill
