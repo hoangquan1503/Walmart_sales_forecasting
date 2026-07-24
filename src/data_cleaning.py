e9b04b6d018cbee96f65c4c983e2ff23b2b2b890
hoangquan1503 <hoangquan.20060315.808@gmail.com>
2026-07-24 23:59:24 +0700

    fix param of fill_missing func

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -6,7 +6,7 @@ def check_missing_value(df):
         "ratio": df.isna().sum() / df.shape[0]
     })
     return df_na
-def fill_missing_promote(cols):
+def fill_missing_promote(df, cols):
     df_fill = df.copy()
-    df_fill = df_fill.fillna(0)
+    df_fill = df_fill[cols].fillna(0)
     return df_fill
