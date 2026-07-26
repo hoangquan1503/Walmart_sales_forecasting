683ef455e89ec1cd6649059fda477466bd9e8d52
hoangquan1503 <hoangquan.20060315.808@gmail.com>
2026-07-26 22:36:00 +0700

    handle outlier

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -6,7 +6,15 @@ def check_missing_value(df):
         "ratio": df.isna().sum() / df.shape[0]
     })
     return df_na
+
 def fill_missing_promote(df, cols):
     df_fill = df.copy()
     df_fill[cols] = df_fill[cols].fillna(0)
     return df_fill
+
+def corrected_outlier(df, factor=3):
+    df_corrected = df.copy()
+    z_score = (df_corrected['Weekly_Sales'] - df_corrected['Weekly_Sales'].mean()) / df_corrected['Weekly_Sales'].std()
+    outlier = z_score > factor
+    df_corrected.loc[outlier, 'Weekly_Sales'] = df_corrected.median()
+    return df_corrected
