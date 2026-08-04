16c2234da3b4713670b95a19cceb4ef5549e53b3
hoangquan1503 <hoangquan.20060315.808@gmail.com>
2026-08-04 15:31:59 +0700

    fix wape metric

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def weighted_absolute_percentage_error(y_true, y_pred):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
    
     if np.sum(np.abs(y_true)) == 0:
         return 0.0
