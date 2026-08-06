42a0b43f86a97f881f338a36cf7e6674bad62679
hoangquan1503 <hoangquan.20060315.808@gmail.com>
2026-08-06 23:16:10 +0700

    add plot actual vs pred

--- src/plots.py
+++ src/plots.py
@@ -17,7 +17,7 @@ def plot_sales(df, store_id=1, dept_id=1):
             ax.annotate(
                 "-",
                 xy=(date, sale),
-                color="red",  # Set text color to red
+                color="red",  
                 size=20,
             )
     ax.set_xlabel("Date")
@@ -25,3 +25,34 @@ def plot_sales(df, store_id=1, dept_id=1):
     ax.set_title(f"Store ID: {store_id} - Dept ID: {dept_id}")
     ax.legend()
     plt.show()
+
+
+def visualize_pred(pred_dict):
+    final_df = []
+    for store_dept, pred in pred_dict:
+        pred = pred.copy()
+        pred['store_dept'] = store_dept
+        final_df.append(pred)
+    return pd.concat(final_df, ignore_index=True)
+
+
+def plot_prophet_comparison(pred, store_dept):
+    df = pred[pred['store_dept'] == store_dept]
+    if df.empty:
+        print('No data to plot')
+        return
+    plt.figure(figsize=(12,6))
+    sns.lineplot(data=df, x="ds", y="y", label="Actual", color="black")
+    sns.lineplot(data=df, x="ds", y="yhat", label="Forecast", color="blue")
+    plt.fill_between(df['ds'], df['yhat_lower'], df['yhat_upper'], color='blue', alpha=0.2, label='Confidence Interval')
+    plt.title(f'{store_dept} Prediction vs True Value')
+    plt.xlabel('Date')
+    plt.ylabel('Sales')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+    
+    
